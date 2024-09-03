134894fda27fc6f5a24eac74d7d97afb947195d0
Mayderking <163465498+Mayderking@users.noreply.github.com>
2024-09-03 18:11:16 -0500

    commit 3
    se introduce informacion en models, templates etc...

--- gestion_inventarios/inventario/models.py
+++ gestion_inventarios/inventario/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
-# Create your models here.
-
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
@@ -21,16 +17,20 @@ class Proveedor(models.Model):
     def __str__(self):
         return str(self.nombre)
 
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.nombre)
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.SET_NULL, null=True)
+    etiquetas = models.ManyToManyField(Etiqueta)
     cantidad = models.IntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     descripcion = models.TextField()
 
     def __str__(self):
         return str(self.nombre)
-
-
-    
